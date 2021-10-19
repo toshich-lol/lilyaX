b69e5f1a39a65a52948070ce1109b7b38fd573f2
Antoxa from kamenka <fpm.panulin@bsu.by>
2021-10-20 00:16:17 +0300

    UFFFF PERFECT

--- laba5.cpp
+++ laba5.cpp
@@ -7,8 +7,7 @@ int main() {
 	string enter_path = "enter_file.txt";
 	string exit_path = "exit_file.txt";
 	string str_first, temp_string;
-	int number_of_string = 0;
-	int value_of_repeats = 1;
+	int number_of_string = 0, value_of_repeats = 1, n = 0;
 	ifstream enter_file;
 	ofstream exit_file;
 	enter_file.open(enter_path);
@@ -32,7 +31,9 @@ int main() {
 			}
 		}
 		else {
-			exit_file << value_of_repeats << " \n";
+			if (n > 0){
+				exit_file << value_of_repeats << " \n";
+			}
 			value_of_repeats = 1;
 			for (int i = 0; i < str_first.length(); i++) {
 				if (str_first[i] < 48 || str_first[i] > 57) {
@@ -44,6 +45,7 @@ int main() {
 				exit_file << str_first << " " << number_of_string << " ";
 			}
 		}
+		n++;
 		if (str_first.empty())
 			continue;
 		temp_string = str_first;
