acc04d5cffb2495b6465b730bac8c879b9057cac
Antoxa from kamenka <fpm.panulin@bsu.by>
2021-11-04 09:45:42 +0300

    all is ok

--- ax^2+bx+c=0/ax^2+bx+c=0.cpp
+++ ax^2+bx+c=0/ax^2+bx+c=0.cpp
@@ -12,7 +12,6 @@ int main(){
 	cout << endl << "c = ";
 	cin >> c;
 	desision(a, b, c);
-	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 	run_tests();
 	return 0;
 }
