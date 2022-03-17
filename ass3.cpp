d9fe66a07908dde643e3ac61c0a1a4d0c733cc86
Antoxa from kamenka <fpm.panulin@bsu.by>
2022-03-17 21:55:58 +0300

    3 and 4

--- ass3.cpp
+++ ass3.cpp
@@ -1,8 +1,8 @@
 ﻿
 #include <iostream>
 
-int main()
-{
+int main(){
+
     int number;
 
     __int32 a, numenator, denominator;
@@ -45,29 +45,99 @@ int main()
         std::cout << "ASS result: "<< std::endl;
         break;
     case 3:
+
+        int fib[48];
         __asm {
-        
+
+            xor EAX, EAX
+            xor ECX, ECX
+            mov [fib + 4 * ECX], EAX
+            inc ECX
+            mov[fib + 4 * ECX], ECX
+
+        _begin:
+
+            add EAX, [fib + 4 * ECX]
+            jc _end
+            inc ECX
+            mov [fib + 4 * ECX], EAX
+            mov EAX, [fib + 4 * ECX - 4]
+            jmp _begin
+
+        _end:
+
         }
 
-        std::cout << "ASS result: "  << std::endl;
+        for (int i = 0; i < 47; i++){
+            std::cout << fib[i] << ", ";
+        }
         break;
     case 4:
         std::cout << "Enter numenator and denominator:\n";
 
         std::cin >> numenator >> denominator;
 
+
         if (denominator <= 0)
         {
-            std::cout << "Incorrecn denominator";
+            std::cout << "Incorrect denominator";
             throw;
-
-            __asm {
-
-            }
         }
+
+        __asm {
+            mov EAX, numenator
+            mov EBX, denominator
+            xor EDX, EDX
+
+            cmp EAX, 0
+            jg begin_
+            neg EAX
+
+        begin_:
+
+            cmp EAX, EBX
+            je end_ 
+            jg gr 
+            sub EBX, EAX
+            xor EDX, EDX
+            jmp begin_
+
+        gr:
+
+            sub EAX, EBX
+            xor EDX, EDX
+            jmp begin_
+
+        end_:
+            
+            mov ECX, EAX
+
+            mov EAX, denominator
+            div ECX
+            mov denominator, EAX
+
+            mov EAX, numenator
+            xor EDX, EDX
+            cmp numenator, 0
+            jg finish
+
+            neg EAX
+            div ECX
+            neg EAX
+            jmp p1
+
+        finish:
+
+            div ECX
+
+        p1:
+
+            mov numenator, EAX
+        }
+        std::cout << numenator << "/" << denominator << std::endl;
         break;
     default:
-        std::cout << "Incorrecn number";
+        std::cout << "Incorrect number";
         throw;
     }
     return 0;
